1c9c78d558453f88733fe4d589f6c5b17b9a7282
Dillon Ledford <dillon.ledford.dev@gmail.com>
2026-04-23 13:28:17 -0500

    Update server.js
    Minor contact fix

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post('/api/contact', async (req, res) => {
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to: process.env.EMAIL_USER,
-		subject: `Portfolio Contact from ${name} (${email})`,
+		subject: `Portfolio Contact: ${name} (${email})`,
         text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`
     };
 
