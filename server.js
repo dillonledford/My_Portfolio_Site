1f205887d13896ab21c5168fc26e2aab17780c5e
Dillon Ledford <dillon.ledford.dev@gmail.com>
2026-05-14 23:36:29 -0500

    fixes

--- server.js
+++ server.js
@@ -38,32 +38,14 @@ app.post('/api/contact', async (req, res) => {
     }
 
     // Send email - Backup original
-    // const transporter = nodemailer.createTransport({
-        // service: 'gmail',
-        // auth: {
-            // user: process.env.EMAIL_USER,
-            // pass: process.env.EMAIL_PASS
-        // }
-    // });
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
 
-	
-// ------ Send email  --------------  //
-	const transporter = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: 587,
-		secure: false,
-		auth: {
-			user: process.env.EMAIL_USER,
-			pass: process.env.EMAIL_PASS
-		},
-		tls: {
-			rejectUnauthorized: false
-		},
-		dnsTimeout: 10000,
-		socketTimeout: 10000,
-		greetingTimeout: 10000
-	});
-// -------- email end ----//
 
     const mailOptions = {
         from: process.env.EMAIL_USER,
